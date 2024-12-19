5d691c597f95c4660c58b537d8eeef02c711764a
sourav das <sd1535060@gmail.com>
2024-12-19 10:15:28 +0530

    log in and view1 page
    a simple interface of login and view page

---
+++ login.js
@@ -0,0 +1,30 @@
+var count = 2;
+
+function validate() {
+   var user = document.login.username.value;
+   var password = document.login.password.value;
+   var valid = false;
+   var usernameArray = ["student"]
+   var passwordArray = ["1234"]
+   for (var i = 0; i < usernameArray.length; i++)
+
+      if (user == usernameArray[i])
+         if (password == passwordArray[i]) {
+            valid = true;
+            break;
+         }
+
+   if (valid) {
+      alert("Login was successful");
+      window.location = "wwww.google.ie"
+      return false;
+   }
+   var again = "tries";
+   if (count == 1) {
+      again = "try"
+   }
+   if (count >= 1) {
+      alert("Wrong password or username")
+      count--;
+   }
+}
