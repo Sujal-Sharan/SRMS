4f86a921ab7cc6c2e2148e33bc792946fe4ccfc7
sourav das <sd1535060@gmail.com>
2024-12-22 11:33:20 +0530

    connect to view.html
    connected to view.html using windows.location.href="view.html";

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,4 +1,4 @@
-var count = 2;
+/*var count = 2;
 
 function validate() {
    var user = document.login.username.value;
@@ -27,4 +27,40 @@ function validate() {
       alert("Wrong password or username")
       count--;
    }
+}*/
+var count = 2;
+
+function validate() {
+    var user = document.login.username.value;
+    var password = document.login.password.value;
+    var valid = false;
+    var usernameArray = ["student"];
+    var passwordArray = ["1234"];
+    
+    for (var i = 0; i < usernameArray.length; i++) {
+        if (user == usernameArray[i] && password == passwordArray[i]) {
+            valid = true;
+            break;
+        }
+    }
+
+    if (valid) {
+        alert("Login was successful");
+        window.location.href = "view.html"; // Redirect to view.html after successful login
+        return false;
+    }
+    
+    var again = "tries";
+    if (count == 1) {
+        again = "try";
+    }
+    if (count >= 1) {
+        alert("Wrong password or username. You have " + count + " " + again + " left.");
+        count--;
+    } else {
+        alert("You have exceeded the number of login attempts.");
+        document.login.username.disabled = true;
+        document.login.password.disabled = true;
+        document.login.submit.disabled = true;
+    }
 }
