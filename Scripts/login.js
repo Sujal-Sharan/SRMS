446af118ff25fd8f5bc01b25d3e121cebcfabcb1
Sujal_Sharan <sujalsharan2007@gmail.com>
2025-01-15 23:22:03 +0530

    Established DB connection

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,51 +1,11 @@
-/*var count = 2;
-
-function validate() {
-   var user = document.login.username.value;
-   var password = document.login.password.value;
-   var valid = false;
-   var usernameArray = ["student"]
-   var passwordArray = ["1234"]
-   for (var i = 0; i < usernameArray.length; i++)
-
-      if (user == usernameArray[i])
-         if (password == passwordArray[i]) {
-            valid = true;
-            break;
-         }
-
-   if (valid) {
-      alert("Login was successful");
-      window.location = "wwww.google.ie"
-      return false;
-   }
-   var again = "tries";
-   if (count == 1) {
-      again = "try"
-   }
-   if (count >= 1) {
-      alert("Wrong password or username")
-      count--;
-   }
-}*/
 var count = 2;
 
 function validate() {
 
-   var username = document.login.username.value;
-   var password = document.login.password.value;
-   //var valid = false;
-    //var usernameArray = ["student"];
-    //var passwordArray = ["1234"];
+   var username = document.login.username.value;    //gets 'username' from the form
+   var password = document.login.password.value;    //gets 'password' from the form (has to be hashed for security)
 
-   const user = users.find((u) => u.username === username && u.password === password);
-    
-   //  for (var i = 0; i < usernameArray.length; i++) {
-   //      if (user == usernameArray[i] && password == passwordArray[i]) {
-   //          valid = true;
-   //          break;
-   //      }
-   //  }
+   const user = users.find((u) => (u.username === username) && (u.password === password));  //matching if user exists and password matches
 
    if (user) {
       alert("Login was successful");
@@ -53,44 +13,22 @@ function validate() {
       return false;
    }
     
-   var again = "tries";
-   if (count == 1) {
-      again = "try";
-   }
    if (count >= 1) {
-      alert("Wrong password or username. You have " + count + " " + again + " left.");
+      alert("Wrong password or username. You have " + count + " attempt(s) left.");
       count--;
-   } else {
-      alert("You have exceeded the number of login attempts.");
+   }
+   else {
+      alert("You have exceeded the number of login attempts. Further login attempts have been disabled");
       document.login.username.disabled = true;
       document.login.password.disabled = true;
       document.login.submit.disabled = true;
-   }
+   }    //maybe add 'count = 2' again in else part to reset count after 3 failed attempts for new user?
 }
 
-
-
-// Predefined list of users
+// Predefined list of users, to be replaced with a database
 const users = [
    { username: "admin", password: "admin123" },
    { username: "user1", password: "password1" },
    { username: "user2", password: "password2" },
  ];
 
-// const user = users.find((u) => u.username === username && u.password === password);
-
-
-// HashTable Implementation
-// class HashTable {
-//    constructor() {
-//        this.table = new Array(10);
-//        this.size = 0;
-//    }
-
-
-//    contains(){
-//         // Check if the user exists in the predefined list
-//       // const user = users.find((u) => u.username === username && u.password === password);
-//    }
-
-// }
